9cdda71a19941f9557e1defb36c06babd049b81f
daef <daef@sourcemonk.com>
2015-07-01 14:46:10 +0200

    jetzt gibt er den schädel und die schätzung im modelraum als vtk aus und sie koennen im viewer geladen werden +model parameter

--- zygo.R
+++ zygo.R
@@ -1,4 +1,6 @@
 #!/usr/bin/r
+
+
 require(Rvcg)
 require(Morpho)
 require(mesheR)
@@ -6,19 +8,20 @@ require(RvtkStatismo)
 
 if (!require(getopt))
     install.packages("getopt",repos="http://cran.rstudio.com/",quiet=TRUE)
+require(getopt)
 spec = matrix(c(
-                'skull',     's', 1, "character",
-                'landmarks', 'l', 1, "character",
-                'output' ,   'o', 1, "character",
-                'iterations', 'i', 2, "integer",
-                'help'   , 'h', 0, "logical"
-                ), byrow=TRUE, ncol=4);
+    'skull',     's', 1, "character",
+    'landmarks', 'l', 1, "character",
+    'output' ,   'o', 1, "character",
+    'iterations', 'i', 2, "integer",
+    'help'   , 'h', 0, "logical"
+    ), byrow=TRUE, ncol=4);
 opt <- getopt(spec)
 if ( !is.null(opt$help) || is.null(opt$landmarks) || is.null(opt$skull) ) {
-    cat(getopt(spec, usage=TRUE,,command="zygo.R"));
-    cat("   estimates a pointcloud of the soft-tissue nose\n")
-    q(status=1);
-}
+       cat(getopt(spec, usage=TRUE,,command="zygoForDave.r"));
+       cat("   estimates a pointcloud of the soft-tissue nose\n")
+       q(status=1);
+   }
 
 skulllm <- read.pts(opt$landmarks)
 modlm <- read.pts("data/zlmr.pts")
@@ -43,24 +46,26 @@ myfun <- function(skull,skulllm,sdmax=NULL,iterations=10,icp=TRUE,matching="r",.
     initrot <- computeTransform(modlm,skulllm,type = "r")
     skullrot <- skullrot2 <- applyTransform(skull,initrot)
     skullrotlm <- skullrotlm2 <- applyTransform(skulllm,initrot)
-
+    
     if (icp) {
         zygoicp <- icp(DrawMean(newmodRed),skullrot,iterations = 30,getTransform = T,type=matching,subsample=1000,rho=pi/2,uprange = 0.5)
         skullrot2 <- applyTransform(skullrot,zygoicp$transform,inverse = T)
         skullrotlm2 <- applyTransform(skullrotlm,zygoicp$transform,inverse = T)
     }
     cModRed <- statismoConstrainModel(newmodRed,skullrotlm2,modlm,ptValueNoise = 2)
-    #wiredeform <- modelFitting(cMod,skullrot2,tardist = 4,refdist = 6,iterations = 10)
+                                        #wiredeform <- modelFitting(cMod,skullrot2,tardist = 4,refdist = 6,iterations = 10)
     wiredeformRed <- modelFitting(cModRed,skullrot2,tardist = 3,refdist = 6 ,iterations = iterations,sdmax = sdmax,mahaprob = "c",lbfgs.iter = 15,...)
     if (icp)
         finalRed <- applyTransform(wiredeformRed$mesh,zygoicp$transform)
     else
         finalRed <- wiredeformRed$mesh
     finalorigRed <- applyTransform(finalRed,initrot,inverse = T)
-    return(list(estimate=finalorigRed,mod=cModRed))
+    return(list(estimate=finalorigRed,mod=cModRed,par=wiredeformRed$par,skullrot=skullrot2,estimrot=wiredeformRed$mesh))
 }
 
 out <- myfun(skull,skulllm,iterations=iterations,sdmax=4,orthantwise_c=0.01)
 vcgPlyWrite(out$estimate,opt$output)
-# statismoSaveModel(out$mod,modelname=paste0(opt$output,".h5"))
-
+vtkMeshWrite(out$skullrot,paste0(opt$output,"_skull_rotated"))
+vtkMeshWrite(out$estimrot,paste0(opt$output,"_estimation_rotated"))
+statismoSaveModel(out$mod,modelname=paste0(opt$output,".h5"))
+write.table(out$par,col.names=F,row.names=F,file=paste0(opt$output,"_params.csv"))
